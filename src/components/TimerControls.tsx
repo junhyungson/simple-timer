00a4faa28678a01476ad3d32083131e7189c6471
junhyungson <cybergeron89@gmail.com>
2025-08-19 13:20:32 -0700

    add focus function

--- src/components/TimerControls.tsx
+++ src/components/TimerControls.tsx
@@ -1,12 +1,22 @@
+import { useEffect, useRef } from "react";
+
 interface TimerControsProps {
   onToggle: () => void;
   isRunning: boolean;
   onReset: () => void;
 }
 const TimerControls = ({ onToggle, isRunning, onReset }: TimerControsProps) => {
+  const startButtonRef = useRef<HTMLButtonElement>(null);
+
+  useEffect(() => {
+    if (startButtonRef.current) {
+      startButtonRef.current.focus();
+    }
+  }, []);
   return (
     <div>
       <button
+        ref={startButtonRef}
         onClick={onToggle}
         className="mt-3 mr-3  bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600"
       >
